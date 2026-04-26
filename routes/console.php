846cdd8d71e4c4f2fefb9048807c40ea987a3e41
ihemaax <ihemaax@gmail.com>
2026-04-26 03:18:31 -0700

    Fix product pagination ordering and keep current page on edit/delete

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use Database\Seeders\TheTreeRestaurantMenuSeeder;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('menu:import-thetree', function () {
+    $this->call('db:seed', ['--class' => TheTreeRestaurantMenuSeeder::class, '--force' => true]);
+})->purpose('Import full menu data for The Tree restaurant (restaurant_id=5).');
